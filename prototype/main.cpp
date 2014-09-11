bbb3fd68f93d7e3f78db24fc906ff9e27305bc3c
landon noss <landongn@gmail.com>
2014-09-10 21:01:31 -0700

    init

---
+++ prototype/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  prototype
+//
+//  Created by thock on 9/10/14.
+//  Copyright (c) 2014 onlyfunthings. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
